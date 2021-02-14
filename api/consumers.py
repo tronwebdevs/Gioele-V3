07939ab7299546d588b94ff6abb82e907a6785c1
Davide Testolin <tdavide@protonmail.com>
2021-02-14 20:48:31 +0100

    Add giorgio's requests handlers + code refractor

--- api/consumers.py
+++ api/consumers.py
@@ -4,80 +4,168 @@ import uuid
 from channels.generic.websocket import WebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 
-from .game import Game
+from .game import Giorgio
+
+ACTION_GAME_START = 0
+ACTION_GAME_PAUSE = 1
+ACTION_GAME_STOP = 2
+ACTION_ENEMY_HIT_PLAYER_DIR = 3
+ACTION_ENEMY_HIT_PLAYER_IND = 4
+ACTION_ENEMY_HIT_MSHIP = 5
+ACTION_PLAYER_HIT_ENEMY = 6
+ACTION_PLAYER_GAIN_POWERUP = 7
+ACTION_PLAYER_USE_ABILITY = 8
+
+ERROR_INVALID_ACTION = 1
+ERROR_GAME_NOT_STARTED = 2
+ERROR_GAME_ALREADY_STARTED = 3
+ERROR_ENTITY_NOT_FOUND = 4
 
 
 class GameConsumer(WebsocketConsumer):
 
     def connect(self):
-        if type(self.scope["user"]) is AnonymousUser:
+        if type(self.scope['user']) is AnonymousUser:
             print('Unauthenticated user')
             self.disconnect(1008)
             return
-        print(self.scope["user"].user.username + ' connected')
-        self.scope["game"] = None
+        print(self.scope['user'].user.username + ' connected')
+        self.scope['game'] = None
         self.accept()
 
     def disconnect(self, close_code):
-        if type(self.scope["user"]) is not AnonymousUser:
-            if self.scope["game"] is not None:
-                self.scope["game"].save(uuid.UUID("5c9539b514a9442ab5e14ae6ec50ccdb"))
-            print(self.scope["user"].user.username + ' disconnected')
+        if type(self.scope['user']) is not AnonymousUser:
+            if self.scope['game'] is not None:
+                self.scope['game'].save(uuid.UUID('5c9539b514a9442ab5e14ae6ec50ccdb'))
+            print(self.scope['user'].user.username + ' disconnected')
 
     def validate_data(self, text_data):
         data = json.loads(text_data)
-        if 'a' not in data:
-            raise Exception('Invalid data (no a)')
-        if type(data['a']) is not int or data['a'] < 0 or data['a'] > 10:
+        action = data.get('a')
+        if type(action) is not int or action < 0 or action > 8:
             raise Exception('Invalid data (a)')
-        return data, data['a']
+        _id = None
+        if action > 2:
+            _id = data.get('i')
+            if type(_id) is not int:
+                raise Exception('Invalid data (i)')
+        return data, action, _id
+
+    def send_dict(self, val):
+        self.send(text_data=json.dumps(val))
 
     def send_error(self, message, code=-1):
-        self.send(text_data=json.dumps({
+        self.send_dict({
             'code': code,
             'message': message
-        }))
+        })
+
+    def get_entity(self, action, eid):
+        exception = Exception('Entity not found')
+        game = self.scope['game']
+        if action < 7:
+            # Entity is an enemy
+            entity = game.enemies.get(eid)
+        elif action == 7:
+            # Entity is a powerup
+            entity = game.player.active_powerups.get(eid)
+        elif action == 8:
+            # Entity is an ability
+            entity = None #game.player.abilites.get(eid)
+        else:
+            raise exception
+        
+        if entity is None:
+            raise exception
+        return entity
 
     def receive(self, text_data):
         try:
-            data, action = self.validate_data(text_data)
+            data, action, obj_id = self.validate_data(text_data)
         except Exception as e:
             self.send_error(str(e))
             return
-        game = self.scope["game"]
-        """
-        'a': action type, possible values:
-            - 0: game start
-            - 1: game stop
-            - 5: bullet shooted
-            - 10: enemy hit
-        """
+        game = self.scope['game']
+        user = self.scope['user']
         
-        if action == 0 and game is None:
+        if action == ACTION_GAME_START:
+            if game is None:
+                self.send_error('Game already started', ERROR_GAME_ALREADY_STARTED)
+                return
             try:
-                self.scope["game"] = Game(
-                    self.scope["user"].user.id,
-                    'yAiL',
-                    'oBkq'
+                self.scope['game'] = Giorgio(
+                    user.user.id,
+                    user.inventory.abilites,
+                    user.main_gun,
+                    user.side_gun,
+                    user.skin
                 )
                 print('Game started')
             except Exception as e:
                 self.send_error(str(e))
         elif game is not None:
-            if action == 1:
-                # TODO: game stopped, store on db
+            player = game.player
+            
+            if action > 2:
+                # Check if given enemy's id is valid and get enemy object from stack
+                try:
+                    enemy = game.get_enemy(obj_id)
+                except Exception as e:
+                    self.send_error(str(e), ERROR_ENTITY_NOT_FOUND)
+                    return
+            # Switch between actions
+            if action == ACTION_GAME_PAUSE:
+                # TODO: game paused, stop all scheds
                 pass
-            elif action == 5:
-                if data['t'] == 0:
-                    game.shooted_main += 1
-                    print(f'Shooted main: {game.shooted_main}')
-                elif data['t'] == 1:
-                    game.shooted_side += 1
-                    print(f'Shooted side: {game.shooted_side}')
-            elif action == 10:
-                # TODO: check if enemy has been killed and act accordingly
+            elif action == ACTION_GAME_STOP:
+                # TODO: game stopped, store on db
+                self.send_error('Game ended', 0)
+            elif action == ACTION_ENEMY_HIT_PLAYER_DIR:
+                # TODO: remove the enemy from the stack, check if player died (trigger end game) or just remove xp (or shield) from player
+                pass
+            elif action == ACTION_ENEMY_HIT_PLAYER_IND:
+                # TODO: check for player death or remove xp (or sheild)
+                pass
+            elif action == ACTION_ENEMY_HIT_MSHIP:
+                # Remove enemy from stack
+                del game.enemies[obj_id]
+                temp = game.mship_lifes - 1
+                if temp < 0:
+                    # Mother ship is dead, game ends
+                    # TODO: game end
+                    self.send_error('Game ended', 0)
+                else:
+                    # Mother ship lost a life
+                    game.mship_lifes = temp
+                    self.send_dict({ 'lifes': game.mship_lifes })
+            elif action == ACTION_PLAYER_HIT_ENEMY:
+                # Check if given gun's type is valid
+                gun_type = data.get('t')
+                if type(gun_type) is not int or gun_type < 0 or gun_type > 1 or (gun_type == 1 and player.side_gun is None):
+                    self.send_error('Invalid data (t)')
+                    return
+                # Increment hit bullets' counter
+                player.bullet_hit()
+                # Calculate enemy's remained hp
+                temp = enemy.hp - player.get_damage(gun_type)
+                if temp < 0:
+                    # enemy is dead
+                    game.killed.add(enemy.type)
+                    # Remove enemy from stack
+                    del game.enemies[obj_id]
+                    enemy.hp = 0
+                else:
+                    # enemy has lost hp
+                    game.enemies[obj_id].hp = temp
+                # Return updated enemy
+                self.send_dict(enemy)
+            elif action == ACTION_PLAYER_GAIN_POWERUP:
+                # TODO: Add powerup to player's list and start timer (which at end remove) the powerup from every stack)
+                pass
+            elif action == ACTION_PLAYER_USE_ABILITY:
+                # TODO: Perform the ability effects
                 pass
             else:
                 self.send_error('Invalid action')
         else:
-            self.send_error('Game hasn\'t started')
+            self.send_error('Game hasn\'t started', ERROR_GAME_NOT_STARTED)
--- api/game.py
+++ api/game.py
@@ -1,44 +1,70 @@
+import sched
 from uuid import uuid4
 
 from django.utils import timezone
 
-from .models import Gun, GUser, GameLog, VisitLog
+from .models import Gun, Skin, GUser, GameLog, VisitLog
 from .classes import Parser
+
+MAX_PLAYER_HP = 100
+MAX_MSHIP_LIFES = 3
 
 parser = Parser()
 
 
-class PowerUp:
-    TYPES = [
-        'pu_type_0', 'pu_type_1', 'pu_type_2', 'pu_type_3'
-    ]
-
-    def __init__(self, _type):
-        self.type = _type
+class Entity:
+    def __init__(self, id, type):
+        self.id = id
+        self.type = type
 
 
-class Enemy:
-    TYPES = [
-        'enemy_type_0', 'enemy_type_1', 'enemy_type_2', 'enemy_type_3'
-    ]
+class PowerUp(Entity):
+    TYPES = ['pu_type_0', 'pu_type_1', 'pu_type_2', 'pu_type_3']
 
-    def __init__(self, xp, _type):
-        self.xp = xp
-        self.type = _type
+    def __init__(self, id, type):
+        super().__init__(id, type)
+
+
+class Ability(Entity):
+    TYPES = ['a_type_0', 'a_type_1', 'a_type_2', 'a_type_3']
+
+    def __init__(self, id, type):
+        super().__init__(id, type)
+
+
+class Enemy(Entity):
+    TYPES = ['enemy_type_0', 'enemy_type_1', 'enemy_type_2', 'enemy_type_3']
+    RARITIES = {
+        'base': 0,
+        'rare': 1,
+        'epic': 2,
+        'mytic': 3,
+        'leggendary': 4,
+    }
+
+    def __init__(self, id, type, hp, damage, rarity=RARITIES['base'], is_boss=False):
+        super().__init__(id, type)
+        self.hp = hp
+        self.damage = damage
+        self.rarity = rarity
+        self.is_boss = is_boss
+
+    def __str__(self):
+        return f'{self.id}({self.type}): {self.xp} [{self.rarity}]'
 
 
 class Manager:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, types):
+        self.data = { types[i]: 0 for i in range(len(types)) }
 
-    def get(self, id):
-        return self.data[id]
+    def get(self, _id):
+        return self.data.get(_id)
 
-    def add(self, id):
-        self.data[id] += 1
+    def add(self, _id):
+        self.data[_id] += 1
 
-    def remove(self, id):
-        self.data[id] -= 1
+    def remove(self, _id):
+        self.data[_id] -= 1
 
     def __str__(self):
         return parser.dict_to_string(self.data)
@@ -46,36 +72,62 @@ class Manager:
 
 class PowerUpManager(Manager):
     def __init__(self):
-        super().__init__({PowerUp.TYPES[i]: 0 for i in range(0, len(PowerUp.TYPES))})
+        super().__init__(PowerUp.TYPES)
 
 
 class EnemyManager(Manager):
     def __init__(self):
-        super().__init__({Enemy.TYPES[i]: 0 for i in range(0, len(Enemy.TYPES))})
+        super().__init__(Enemy.TYPES)
+
+class AbilityManager(Manager):
+    def __init__(self):
+        super().__init__(Ability.TYPES)
 
 
 class Player:
     expired_powerups = PowerUpManager()
     active_powerups = PowerUpManager()
+    used_abilities = AbilityManager()
     shield_active = False
-
-    def __init__(self, user_id):
-        self.user_id = user_id
-
-
-class Game:
-    id = uuid4()
+    hp = MAX_PLAYER_HP
     exp = 0
     gbucks = 0
-    shooted_main = 0
-    shooted_side = 0
-    start_time = timezone.now()
-    killed = EnemyManager()
+    main_hit = 0
+    side_hit = 0
 
-    def __init__(self, user_id, main_gun_id, side_gun_id):
-        self.player = Player(user_id)
+    def __init__(self, user_id, abilities, main_gun_id, side_gun_id, skin_id):
+        self.user_id = user_id
+        self.abilities = []
         self.main_gun = Gun.objects.get(pk=main_gun_id)
-        self.side_gun = Gun.objects.get(pk=side_gun_id)
+        if side_gun_id is not None:
+            self.side_gun = Gun.objects.get(pk=side_gun_id)
+        else:
+            self.side_gun = None
+        self.skin = Skin.objects.get(pk=skin_id)
+
+    def bullet_hit(self, gun_type):
+        if gun_type == 0:
+            self.main_hit += 1
+        else:
+            self.side_hit += 1
+
+    """
+    Get hp damage from a specified gun type.
+    Remember to check first if side_gun is not null.
+    """
+    def get_damage(self, gun_type):
+        return self.main_gun.damage if gun_type == 0 else self.side_gun.damage
+
+
+class Giorgio:
+    game_id = uuid4()
+    start_time = timezone.now()
+    enemies = dict()
+    killed = EnemyManager()
+    mship_lifes = MAX_MSHIP_LIFES
+
+    def __init__(self, user_id, abilities, main_gun_id, side_gun_id, skin_id):
+        self.player = Player(user_id, abilities, main_gun_id, side_gun_id, skin_id)
 
     def enemy_hit(self, gun_type, enemy_id):
         
